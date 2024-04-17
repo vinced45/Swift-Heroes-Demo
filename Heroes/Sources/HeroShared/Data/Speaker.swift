2d18433f055b2584028a0f3544067af28d161c75
Davis, Vince <vincent.l.davis@united.com>
2024-04-17 19:26:50 +0200

    Added Speakers

--- Heroes/Sources/HeroShared/Data/Speaker.swift
+++ Heroes/Sources/HeroShared/Data/Speaker.swift
@@ -31,13 +31,142 @@ public extension Speaker {
     }
     
     static var alex: Speaker = .init(id: "1", 
-                                     name: String(localized: "Alex", table: "Heroes", bundle: .module),
-                                     desc: String(localized: "This is Alex", table: "Heroes", bundle: .module),
+                                     name: "Alex",
+                                     desc: "",
                                      image: .alex)
     static var alberto: Speaker = .init(id: "2", 
-                                        name: String(localized: "Alberto", table: "HeroShared", bundle: .module),
-                                        desc: String(localized: "This is Alberto", table: "HeroShared", bundle: .module),
+                                        name: "Alberto",
+                                        desc: "",
                                         image: .alberto)
+    static var andrea: Speaker = .init(id: "3",
+                                        name: "Andrea",
+                                        desc: "",
+                                        image: .andrea)
+    static var andrei: Speaker = .init(id: "4",
+                                        name: "Andrei",
+                                        desc: "",
+                                        image: .andrei)
+    static var batuhan: Speaker = .init(id: "5",
+                                        name: "Batuhan",
+                                        desc: "",
+                                        image: .batuhan)
+    static var beyza: Speaker = .init(id: "6",
+                                        name: "Beyza",
+                                        desc: "",
+                                        image: .beyza)
+    static var borbala: Speaker = .init(id: "7",
+                                        name: "Borbala",
+                                        desc: "",
+                                        image: .borbala)
+    static var brad: Speaker = .init(id: "8",
+                                        name: "Brad",
+                                        desc: "",
+                                        image: .brad)
+    static var davide: Speaker = .init(id: "9",
+                                        name: "Davide",
+                                        desc: "",
+                                        image: .davide)
+    static var davide2: Speaker = .init(id: "10",
+                                        name: "Davide2",
+                                        desc: "",
+                                        image: .davide2)
+    static var emilio: Speaker = .init(id: "11",
+                                        name: "Emilio",
+                                        desc: "",
+                                        image: .emilio)
+    static var flora: Speaker = .init(id: "12",
+                                        name: "Flora",
+                                        desc: "",
+                                        image: .flora)
+    static var gyuree: Speaker = .init(id: "13",
+                                        name: "Gyuree",
+                                        desc: "",
+                                        image: .gyuree)
+    static var jane: Speaker = .init(id: "14",
+                                        name: "Jane",
+                                        desc: "",
+                                        image: .jane)
+    static var josip: Speaker = .init(id: "15",
+                                        name: "Josip",
+                                        desc: "",
+                                        image: .josip)
+    static var krysztof: Speaker = .init(id: "16",
+                                        name: "Krysztof",
+                                        desc: "",
+                                        image: .krysztof)
+    static var libranner: Speaker = .init(id: "17",
+                                        name: "Libranner",
+                                        desc: "",
+                                        image: .libranner)
+    static var lukasz: Speaker = .init(id: "18",
+                                        name: "Lukasz",
+                                        desc: "",
+                                        image: .lukasz)
+    static var marcin: Speaker = .init(id: "19",
+                                        name: "Marcin",
+                                        desc: "",
+                                        image: .marcin)
+    static var marco: Speaker = .init(id: "20",
+                                        name: "Marco",
+                                        desc: "",
+                                        image: .marco)
+    static var mete: Speaker = .init(id: "21",
+                                        name: "Mete",
+                                        desc: "",
+                                        image: .mete)
+    static var michael: Speaker = .init(id: "22",
+                                        name: "michael",
+                                        desc: "",
+                                        image: .michael)
+    static var natan: Speaker = .init(id: "23",
+                                        name: "Natan",
+                                        desc: "",
+                                        image: .natan)
+    static var pedro: Speaker = .init(id: "24",
+                                        name: "Pedro",
+                                        desc: "",
+                                        image: .pedro)
+    static var peter: Speaker = .init(id: "25",
+                                        name: "Peter",
+                                        desc: "",
+                                        image: .peter)
+    static var pietro: Speaker = .init(id: "26",
+                                        name: "Pietro",
+                                        desc: "",
+                                        image: .pietro)
+    static var pol: Speaker = .init(id: "27",
+                                        name: "Pol",
+                                        desc: "",
+                                        image: .pol)
+    static var pradnya: Speaker = .init(id: "28",
+                                        name: "Pradnya",
+                                        desc: "",
+                                        image: .pradnya)
+    static var sara: Speaker = .init(id: "29",
+                                        name: "Sara",
+                                        desc: "",
+                                        image: .sara)
+    static var shai: Speaker = .init(id: "30",
+                                        name: "shai",
+                                        desc: "",
+                                        image: .shai)
+    static var stefano: Speaker = .init(id: "31",
+                                        name: "Stefano",
+                                        desc: "",
+                                        image: .stefano)
+    static var vince: Speaker = .init(id: "31",
+                                        name: "Vince",
+                                        desc: "",
+                                        image: .vince)
+    static var vincenzo: Speaker = .init(id: "32",
+                                        name: "Vincenzo",
+                                        desc: "",
+                                        image: .vincenzo)
+    static var zach: Speaker = .init(id: "33",
+                                        name: "Zach",
+                                        desc: "",
+                                        image: .zach)
+    
 }
 
 extension Speaker: Listable {
